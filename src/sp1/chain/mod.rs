b6fbcd4a05da1cfd71ce209391ed4ad81b133565
Dennis Won <denniswon@users.noreply.github.com>
2025-04-24 16:00:39 -0700

    feat: transaction retry on failure (#30)
    * feat: transaction retry on failure
    
    * feat: added txn retry logics

--- src/sp1/chain/mod.rs
+++ src/sp1/chain/mod.rs
@@ -4,11 +4,11 @@ pub mod attestation;
 pub mod pccs;
 
 use alloy::{
-    network::{EthereumWallet, TransactionBuilder},
+    network::{Ethereum, EthereumWallet, TransactionBuilder},
     primitives::{Address, Bytes, TxHash},
-    providers::{Provider, ProviderBuilder},
+    providers::{PendingTransactionBuilder, Provider, ProviderBuilder},
     rpc::types::{TransactionReceipt, TransactionRequest},
-    signers::{k256::ecdsa::SigningKey, local::PrivateKeySigner, utils::secret_key_to_address}
+    signers::{k256::ecdsa::SigningKey, local::PrivateKeySigner, utils::secret_key_to_address},
 };
 use alloy_chains::NamedChain;
 use anyhow::Result;
@@ -49,45 +49,74 @@ impl TxSender {
             .with_cached_nonce_management()
             .with_gas_estimation()
             .connect_http(rpc_url);
-        let chain_id = provider.get_chain_id().await?;
 
-        let nonce = provider.get_transaction_count(self.account).await?;
-        let tx = TransactionRequest::default()
+        let chain_id = provider.get_chain_id().await?;
+        
+        let tx_request = TransactionRequest::default()
             .with_chain_id(chain_id)
             .with_to(self.contract)
             .with_from(self.account)
-            .with_nonce(nonce + 1)
-            .max_fee_per_gas(20_000_000)
-            .max_priority_fee_per_gas(10_000_000)
-            .with_gas_limit(10_000_000)
             .with_input(calldata);
 
-        let tx_envelope = tx.build(&self.wallet).await?;
-        let tx_hash = tx_envelope.tx_hash().clone();
-        let tx = match provider
-            .send_tx_envelope(tx_envelope.clone())
-            .await {
-                Ok(tx) => {
-                    tracing::info!(
-                        "TxSender: Transaction hash: {} {:#?}",
-                        hex::encode(tx_hash),
-                        tx_envelope.into_typed_transaction()
-                    );
-                    Some(tx)
-                },
-                Err(e) => {
-                    tracing::error!("Failed to send transaction: {} {}", e, hex::encode(tx_hash));
-                    None
-                }
-            };
-        match tx {
+        let mut nonce = provider.get_transaction_count(self.account).await?;
+        let mut multiplier = 1.0;
+        let mut max_retries = 2;
+
+        let mut pending_tx: Option<PendingTransactionBuilder<Ethereum>> = None;
+        let mut transaction_hash: Option<TxHash> = None;
+        loop {
+            nonce += 1;
+            let gas_limit = provider.estimate_gas(tx_request.clone()).await?;
+            let max_fee_per_gas = provider.estimate_eip1559_fees().await?;
+
+            let tx = tx_request.clone()
+                .with_nonce(nonce)
+                .max_fee_per_gas((max_fee_per_gas.max_fee_per_gas as f64 * multiplier) as u128)
+                .max_priority_fee_per_gas((max_fee_per_gas.max_priority_fee_per_gas as f64 * multiplier) as u128)
+                .with_gas_limit((gas_limit as f64 * multiplier) as u64);
+
+            let tx_envelope = tx.build(&self.wallet).await?;
+            let tx_hash = tx_envelope.tx_hash().clone();
+            
+            let tx = match provider
+                .send_tx_envelope(tx_envelope.clone())
+                .await {
+                    Ok(tx) => {
+                        tracing::info!("TxSender: Transaction hash: {}", tx_hash);
+                        tracing::debug!("TxSender: Transaction: {:#?}", tx_envelope.into_typed_transaction());
+                        Some(tx)
+                    },
+                    Err(e) => {
+                        tracing::error!("Failed to send transaction: {} {}", e, tx_hash);
+                        if e.as_error_resp().unwrap().code == -32603 {
+                            tracing::info!("Retrying transaction");
+                            multiplier *= 1.1;
+                            max_retries -= 1;
+                        } else {
+                            tracing::info!("Not retriable error. Skipping retrying transaction: {} {}", e, tx_hash);
+                            max_retries = 0;
+                        }
+                        None
+                    }
+                };
+
+            if tx.is_some() {
+                pending_tx = tx;
+                transaction_hash = Some(tx_hash);
+                break;
+            } else if max_retries == 0 {
+                break;
+            }
+        };
+
+        match pending_tx {
             Some(tx) => {
                 match tx.get_receipt().await {
-                    Ok(receipt) => Ok((tx_hash, Some(receipt))),
-                    Err(_) => Ok((tx_hash, None))
+                    Ok(receipt) => Ok((transaction_hash.unwrap(), Some(receipt))),
+                    Err(_) => Ok((transaction_hash.unwrap(), None))
                 }
             },
-            None => Ok((tx_hash, None))
+            None => Ok((transaction_hash.unwrap(), None))
         }
     }
 
